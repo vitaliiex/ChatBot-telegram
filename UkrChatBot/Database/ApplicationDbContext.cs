df746117637a2c8ff795f4ec17a8dbb21d3f6e2a
vitaliiex <151929039+vitaliiex@users.noreply.github.com>
2024-05-08 10:55:55 +0300

    Update ApplicationDbContext.cs

--- UkrChatBot/Database/ApplicationDbContext.cs
+++ UkrChatBot/Database/ApplicationDbContext.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using UkrChatBot.Models;
+
 namespace UkrChatBot.Database;
 
 public class ApplicationDbContext : DbContext
 {
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Handler>().HasData(
+            new Handler { Id = 1, Title = "start", ClickedCount = 0 },
+            new Handler { Id = 2, Title = "categories", ClickedCount = 0 },
+            new Handler { Id = 3, Title = "dailyrule", ClickedCount = 0 }
+        );
+    }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseNpgsql("Host=localhost;Database=UkrChatBotDb;Username=postgres;Password=postgres");
     }
+
     public DbSet<Handler> Handlers { get; set; }
 }
