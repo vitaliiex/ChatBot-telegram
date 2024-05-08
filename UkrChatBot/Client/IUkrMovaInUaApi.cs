ea521e9c771fa3b5bf885eb28a073e5847356c1b
vitaliiex <151929039+vitaliiex@users.noreply.github.com>
2024-05-08 11:07:34 +0300

    Update IUkrMovaInUaApi.cs

--- UkrChatBot/Client/IUkrMovaInUaApi.cs
+++ UkrChatBot/Client/IUkrMovaInUaApi.cs
@@ -6,5 +6,8 @@ namespace UkrChatBot.Client;
 public interface IUkrMovaInUaApi
 {
     [Get("/api-new?route=categories")]
-    Task<List<Category>> GetCategoriesAsync(CancellationToken cancellationToken = default); 
+    Task<List<Category>> GetCategoriesAsync(CancellationToken cancellationToken = default);
+    
+    [Get("/api-new?route=examples")]
+    Task<List<Example>> GetExamplesAsync(CancellationToken cancellationToken = default);
 }
